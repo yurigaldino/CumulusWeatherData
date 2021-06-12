8d160f2bbc9da9dfc126a3a242259d565e9f9c94
Yuri Galdino <yurifgdesouza@gmail.com>
2021-06-12 11:39:14 -0300

    Creation of header in data.txt, orgaznization and creation (day, time and console battery) variables

--- service/getApiData.js
+++ service/getApiData.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
-let url = 'http://cumulusmx:8998/api/tags/process.json?temp&hum&dew&wspeed&wdir&wgust&recentwindchill&heatindex&THWindex&press&rfall&rrate&heatdegdays&cooldegdays&intemp&inhum&WindSampleCount&forecast';
+let url = 'http://cumulusmx:8998/api/tags/process.json?temp&hum&dew&wspeed&wdir&wgust&recentwindchill&heatindex&THWindex&press&rfall&rrate&heatdegdays&cooldegdays&intemp&inhum&WindSampleCount&forecast&time&date&timehhmmss&battery';
+
 //Api sample data
 /*
 {"temp":"0.0","hum":"0","dew":"0.0","wspeed":"0.0","wdir":"-","wgust":"0.0","recentwindchill":"","heatindex":"0.0","THWindex":"0.0","press":"1020.3","rfall":"618.6","rrate":"0.0","heatdegdays":"0.0","cooldegdays":"0.0","intemp":"28.4","inhum":"57","WindSampleCount":"252","forecast":"Fine weather"}
@@ -14,46 +15,47 @@ const getAsyncData = async () => {
 
     //Full object
     let fullobj = resp.data;
+
     //The outside (air) temperature
-    let temp = resp.data.temp;
+    let temp = fullobj.temp;
     //Maximum temperature
 
     //Min temperature
 
     //The outside humidity
-    let hum = resp.data.hum;
+    let hum = fullobj.hum;
     //The current dew point
-    let dew = resp.data.dew;
+    let dew = fullobj.dew;
     //The wind speed
-    let wspeed = resp.data.wspeed;
+    let wspeed = fullobj.wspeed;
     //The wind direction
-    let wdir = resp.data.wdir;
+    let wdir = fullobj.wdir;
     //The wind run
 
     //Wind highest speed
-    let wgust = resp.data.wgust;
+    let wgust = fullobj.wgust;
     //Wind direction
 
     //Wind chill
-    let recentwindchill = resp.data.recentwindchill;
+    let recentwindchill = fullobj.recentwindchill;
     //Current heat index
-    let heatindex = resp.data.heatindex;
+    let heatindex = fullobj.heatindex;
     //THWIndex
-    let thwindex = resp.data.THWindex;
+    let thwindex = fullobj.THWindex;
     //The sea level pressure
-    let press = resp.data.press;
+    let press = fullobj.press;
     //The total rainfall so far today
-    let rfall = resp.data.rfall;
+    let rfall = fullobj.rfall;
     //The current rainfall rate
-    let rrate = resp.data.rrate;
+    let rrate = fullobj.rrate;
     //Today's heating degree days 
-    let heatdegdays = resp.data.heatdegdays;
+    let heatdegdays = fullobj.heatdegdays;
     //Today's cooling degree days
-    let cooldegdays = resp.data.cooldegdays;
+    let cooldegdays = fullobj.cooldegdays;
     //The inside temperature
-    let intemp = resp.data.intemp;
+    let intemp = fullobj.intemp;
     //The inside humidity
-    let inhum = resp.data.inhum;
+    let inhum = fullobj.inhum;
     //The inside dew point
 
     //The inside heat
@@ -71,16 +73,33 @@ const getAsyncData = async () => {
     //Arc Int
 
     // The number of wind samples making up the wind rose (etc) data (up to 3600) 
-    let WindSampleCount = resp.data.WindSampleCount;
+    let WindSampleCount = fullobj.WindSampleCount;
     //The current forecast
-    let forecast = resp.data.forecast;
+    let forecast = fullobj.forecast;
+
+    //Get current date and time
+    let date_and_time = fullobj.time;
+
+    //Get current date 
+    let date= fullobj.date;
+
+    //Get current time
+    let time= fullobj.timehhmmss;
+
+    //Get current battery
+    let battery= fullobj.battery;
+
+    //Object with just data
+    let formatted_obj = [date, time, temp, hum, dew, wspeed, wdir, wgust, recentwindchill, heatindex, thwindex, press, rfall, rrate, heatdegdays, cooldegdays, intemp, inhum, WindSampleCount, forecast, battery];
+
+    //fs.writeFile(filename, "some data", { flag: "wx" }, function(err)
 
     //Write data in txt
-    fs.writeFile("./data.txt", JSON.stringify(fullobj), 'utf8', function (err) {
+    fs.writeFile("./data.txt", JSON.stringify(formatted_obj) + "\n",{ flag: "a+", encoding: "utf8" },function (err) {
       if (err) {
         return console.log(err);
       }
-      console.log("The file was saved!");
+      console.log("O arquivo foi salvo!");
     });
 
   } catch (err) {
