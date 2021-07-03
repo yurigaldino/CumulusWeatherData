5f0d4ca9627854abe1999776a48f3a554b0e4c2a
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-07-03 12:30:23 -0300

    Add first test to convert data to xml

--- service/convertDataToXml.js
+++ service/convertDataToXml.js
@@ -2,11 +2,27 @@ const fs = require('fs');
 var parser = require('xml2json');
  
 //Read data from txt
- fs.readFile("./data.txt", 'utf8', function (err,data) {
+ fs.readFile("../default_format.xml", 'utf8', function (err,data) {
+    var json = JSON.parse(parser.toJson(data, {reversible: true}));
+    var timeseries = json["weatherdata"]["forecast"]["tabular"]["time"];
+    //console.log(timeseries);
+    for (var i = 0; i < timeseries.length; i++) {
+        var time= timeseries[i];
+        time.temperature.value = i;
+    }
+    
+    var stringified = JSON.stringify(json);
+    var xml = parser.toXml(stringified);
+    fs.writeFile('test.xml', xml, function(err, data) {
+        if (err) {
+        console.log(err);
+        }
+        else {
+        console.log('updated!');
+        }
+    });
+    
     if (err) {
       return console.log(err);
     }
-    console.log(data);
   });
- //Object with just data
-
