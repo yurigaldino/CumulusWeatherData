566d6071ce0cd4ecbb9ed3d4ee593890522649c2
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-07-03 13:08:19 -0300

    Parse txt to array v0

--- service/convertDataToXml.js
+++ service/convertDataToXml.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 var parser = require('xml2json');
  
 //Read data from txt
- fs.readFile("../default_format.xml", 'utf8', function (err,data) {
+/* fs.readFile("../default_format.xml", 'utf8', function (err,data) {
     var json = JSON.parse(parser.toJson(data, {reversible: true}));
     var timeseries = json["weatherdata"]["forecast"]["tabular"]["time"];
-    //console.log(timeseries);
     for (var i = 0; i < timeseries.length; i++) {
         var time= timeseries[i];
         time.temperature.value = i;
@@ -25,4 +24,14 @@ var parser = require('xml2json');
     if (err) {
       return console.log(err);
     }
-  });
+});*/
+
+function getData(){ 
+    var array = fs.readFileSync('./data.txt').toString().split("\n");
+    /*for(i in array) {
+        console.log(array[i]);
+    }*/
+    var array_2 = JSON.parse(array[1]);
+    console.log(array_2[2]); //temperature
+}
+getData();
