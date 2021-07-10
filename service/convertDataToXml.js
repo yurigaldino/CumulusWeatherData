32b08fd2f709ab00f154eb3ff128b905d21b5937
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2021-07-10 11:54:26 -0300

    add a loop to get internal temperature

--- service/convertDataToXml.js
+++ service/convertDataToXml.js
@@ -1,37 +1,42 @@
 const fs = require('fs');
 var parser = require('xml2json');
  
+function setData(data) {
 //Read data from txt
-/* fs.readFile("../default_format.xml", 'utf8', function (err,data) {
-    var json = JSON.parse(parser.toJson(data, {reversible: true}));
-    var timeseries = json["weatherdata"]["forecast"]["tabular"]["time"];
-    for (var i = 0; i < timeseries.length; i++) {
-        var time= timeseries[i];
-        time.temperature.value = i;
-    }
-    
-    var stringified = JSON.stringify(json);
-    var xml = parser.toXml(stringified);
-    fs.writeFile('test.xml', xml, function(err, data) {
-        if (err) {
-        console.log(err);
+    fs.readFile("../default_format.xml", 'utf8', function (err,data) {
+        var json = JSON.parse(parser.toJson(data, {reversible: true}));
+        var timeseries = json["weatherdata"]["forecast"]["tabular"]["time"];
+        for (var i = 0; i < timeseries.length; i++) {
+            var time= timeseries[i];
+            time.temperature.value = i;
         }
-        else {
-        console.log('updated!');
+        
+        var stringified = JSON.stringify(json);
+        var xml = parser.toXml(stringified);
+        fs.writeFile('test.xml', xml, function(err, data) {
+            if (err) {
+            console.log(err);
+            }
+            else {
+            console.log('updated!');
+            }
+        });
+        
+        if (err) {
+        return console.log(err);
         }
     });
-    
-    if (err) {
-      return console.log(err);
-    }
-});*/
+}
 
 function getData(){ 
     var array = fs.readFileSync('./data.txt').toString().split("\n");
-    /*for(i in array) {
-        console.log(array[i]);
-    }*/
-    var array_2 = JSON.parse(array[1]);
-    console.log(array_2[2]); //temperature
+    let internal_temperature =[]
+    //get data of the last 48 data from internal temperature
+    for (let i = array.length - 48; i <array.length - 1; i++) {
+        array_2 = JSON.parse(array[i]);
+        internal_temperature.push(array_2[16]);
+    }
+    return internal_temperature
 }
 getData();
+console.log(getData());
